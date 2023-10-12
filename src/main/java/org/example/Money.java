ec597f8bd87262a73a7b594c01af4d44a5454f2d
mahir <ilijazi.mahir@gmail.com>
2023-10-12 10:32:44 +0200

    TDD - cleaned up constructor calls + implemented currency method

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -2,14 +2,24 @@ package org.example;
 
 public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public String currency() {
+        return currency;
+    }
 
     public abstract Money times(int multiplier);
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     @Override
