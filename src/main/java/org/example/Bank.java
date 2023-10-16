8bd7f24d1ce8cac28415ab623fef19846c8d52a3
mahir <ilijazi.mahir@gmail.com>
2023-10-16 09:00:37 +0200

    TDD - more refactoring & conversion rate added

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Bank {
+    private HashMap<Pair, Integer> rateMap = new HashMap<>();
+
     public Money reduce(Expression source, String toCurrency) {
-        return source.reduce(toCurrency);
-//        if (source instanceof Money) return (Money) source;
-//        Sum sum = (Sum) source;
-//        return sum.reduce(toCurrency);
+        return source.reduce(this, toCurrency);
+    }
+
+    public int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        return rateMap.get(new Pair(from, to));
+//        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
     }
 }
